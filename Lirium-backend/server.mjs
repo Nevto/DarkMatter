6d14d9597240c2585de46be355da2db8803e943d
Nevto <Jonathannorberg95@gmail.com>
2024-05-23 20:45:00 +0200

    Initial commit, added some imports and started server.mjs up

--- Lirium-backend/server.mjs
+++ Lirium-backend/server.mjs
@@ -1,1 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import Lirium from './models/Lirium.mjs';
+
 import PubNubServer from './pubnubServer.mjs';
+
+
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/lirium', liriumRouter)
