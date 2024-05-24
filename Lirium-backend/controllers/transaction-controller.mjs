b99d55b437d81f38a075e84dae3c155b83380517
Nevto <Jonathannorberg95@gmail.com>
2024-05-24 15:08:12 +0200

    did some changes to the transaction functionality

--- Lirium-backend/controllers/transaction-controller.mjs
+++ Lirium-backend/controllers/transaction-controller.mjs
@@ -2,25 +2,25 @@ import { lirium } from "../server.mjs";
 import { pubnubServer } from "../server.mjs";
 
 export const createTransaction = (req, res, next) => {
-    const transaction = req.body;
+    const { amount, sender, recipient } = req.body;
 
-    const index = lirium.addTransaction(transaction);
-
-    res.status(201).json({ success: true, statusCode: 201, data: { message: "Transaction created", transaction, index } });
-};
-
-export const broadcastTransaction = (req, res, next) => {
-    const transaction = lirium.createTransaction(
-        req.body.amount,
-        req.body.sender,
-        req.body.recipient,
-        req.body.transactionId
-    );
+    const transaction = lirium.createTransaction({ amount, sender, recipient });
 
     const index = lirium.addTransaction(transaction);
 
     pubnubServer.broadcast(transaction);
 
-    res.status(201).json({ success: true, statusCode: 201, data: { message: "Transaction created and broadcasted", transaction, index } });
-
+    res.status(201).json({ success: true, statusCode: 201, data: { message: "Transaction created ( test pub)", transaction, index } });
 };
+
+// export const broadcastTransaction = (req, res, next) => {
+// const { amount, sender, recipient } = req.body;
+
+// const transaction = lirium.createTransaction({ amount, sender, recipient });
+
+//     const index = lirium.addTransaction(transaction);
+
+
+//     res.status(201).json({ success: true, statusCode: 201, data: { message: "Transaction created and broadcasted", transaction, index } });
+
+// };
--- Lirium-backend/models/Transaction.mjs
+++ Lirium-backend/models/Transaction.mjs
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export default class Transaction {
-    constructor ({amount, sender, recipient}) {
+    constructor({ amount, sender, recipient }) {
         this.amount = amount;
         this.sender = sender;
         this.recipient = recipient;
