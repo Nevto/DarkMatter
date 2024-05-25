c14524525cc49bf5b030cc6254ea127a67be80bc
Nevto <Jonathannorberg95@gmail.com>
2024-05-25 13:18:01 +0200

    added some googlefonts, working on the design on the transaction page

--- Lirium-frontend/src/components/getLiriumBlocks.jsx
+++ Lirium-frontend/src/components/getLiriumBlocks.jsx
@@ -10,30 +10,38 @@ const RetrieveLiriumBlocks = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleLoadLiriumBlocks}>
-        {' '}
-        Retrieve Blockchain Data
-      </button>
-      <ul>
-        {liriumBlocks.map((block, index) => (
-          <li key={index}>
-            Timestamp: {block.timestamp}
-            <br />
-            Last Hash: {block.lastHash}
-            <br />
-            Hash: {block.hash}
-            <br />
-            Data: {JSON.stringify(block.data)}
-            <br />
-            Nonce: {block.nonce}
-            <br />
-            Difficulty: {block.difficulty}
-            <br />
-            Block Index: {block.blockIndex}
-          </li>
-        ))}
-      </ul>
+    <div className='theWrapper'>
+      <div className='buttonWrapper'>
+        <h2 className='insideText'>Are you ready?</h2>
+        <h3 className='insideText'>take a look at Lirium Protocols fully secure and immutable blocks</h3>
+        <h4 className='insideText'>Get liriumfied</h4>
+        <button className='insideText' onClick={handleLoadLiriumBlocks}>
+          {' '}
+          Retrieve Blockchain Data
+        </button>
+      </div>
+      <div className='blockchainWrapper'>
+        <ul>
+          {liriumBlocks.map((block, index) => (
+            <li key={index} className='block-Container'>
+              Timestamp: {block.timestamp}
+              <br />
+              Last Hash: {block.lastHash}
+              <br />
+              Hash: {block.hash}
+              <br />
+              Data: {JSON.stringify(block.data)}
+              <br />
+              Nonce: {block.nonce}
+              <br />
+              Difficulty: {block.difficulty}
+              <br />
+              Block Index: {block.blockIndex}
+            </li>
+          ))}
+        </ul>
+
+      </div>
     </div>
   );
 };
