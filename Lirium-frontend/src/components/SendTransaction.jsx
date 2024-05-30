9743fcb7f1b40325c76eb0d7f6e9595651d5af8a
Nevto <Jonathannorberg95@gmail.com>
2024-05-30 20:16:07 +0200

    Some more visual updates

--- Lirium-frontend/src/components/SendTransaction.jsx
+++ Lirium-frontend/src/components/SendTransaction.jsx
@@ -35,15 +35,15 @@ const SendTransaction = () => {
     <div className='sendTransactionWrapper'>
       <h1>Send a Transaction</h1>
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className='gap'>
           <label>Sender:</label>
           <input type="text" value={sender} onChange={(e) => setSender(e.target.value)} />
         </div>
-        <div>
+        <div className='gap'>
           <label>Recipient:</label>
           <input type="text" value={recipient} onChange={(e) => setRecipient(e.target.value)} />
         </div>
-        <div>
+        <div className='gap'>
           <label>Amount:</label>
           <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
         </div>
