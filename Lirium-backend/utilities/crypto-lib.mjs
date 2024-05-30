883fcb34b9d458c2640818853f109efeb0aed1f8
VonAdamo <148451831+VonAdamo@users.noreply.github.com>
2024-05-30 20:09:44 +0200

    added a less secure wallet keygen

--- Lirium-backend/utilities/crypto-lib.mjs
+++ Lirium-backend/utilities/crypto-lib.mjs
@@ -7,7 +7,7 @@ export const createHash = (...args) => {
     return crypto.createHash('sha256').update(args.map(arg => JSON.stringify(arg)).sort().join(' ')).digest('hex')
 }
 
-export const ellipticHash = new ec("secp256k1");
+export const ellipticHash = new ec("p192");
 
 export const verifySignature = ({ publicKey, data, signature }) => {
     const key = ellipticHash.keyFromPublic(publicKey, 'hex');
